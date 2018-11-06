b5adaeb99eb8274f3bd51473b56ce6f261533b59
Carlota Fernández <cavefe@gmail.com>
2018-11-06 14:56:36 +0100

    estraidos componentes

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import Dave from "./Dave";
+import Yo from "./Yo";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Dave />
+        <Yo />
+      </div>
+    );
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,24 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-class Dave extends React.Component {
-  render() {
-    return <p>"What do you think you are doing,Dave?</p>;
-  }
-}
+import App from "./components/App";
 
-const Yo = () => {
-  return <p>"What do you think you are doing, Yo?"</p>;
-};
-
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <Dave />
-        <Yo />
-      </div>
-    );
-  }
-}
 ReactDOM.render(<App />, document.querySelector("#app-container"));
